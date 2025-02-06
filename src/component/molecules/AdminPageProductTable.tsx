e5d48a63448a4b62c0a73a75d0f2a35ced10fc74
Metin Baran <metinbozkus0000@gmail.com>
2025-02-06 12:23:12 +0300

    Yeni değişiklikler eklendi

--- src/component/molecules/AdminPageProductTable.tsx
+++ src/component/molecules/AdminPageProductTable.tsx
@@ -57,7 +57,7 @@ function AdminPageProductTable(props: { productList: IAdminProductList[] }) {
                 })
                
               } else {
-                swal("Silme işlemi iptal edildi.");
+                swal("Silme işlemi iptal edildi!");
               }
             });
     }
